414a764e90ccdcee6fe9ab81dc5e039d2aec3833
sudorm12 <sudorm@me.com>
2018-06-06 13:59:37 -0400

    Change y/n columns to boolean and began making categorical columns

--- prepare_data.py
+++ prepare_data.py
@@ -4,9 +4,17 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 applications = pd.read_csv('application_train.csv', index_col="SK_ID_CURR")
+apps_clean = applications.copy()
 
 # TODO: identify encoded columns, change to categorical
-# TODO: change y/n columns to boolean
+
+# change y/n columns to boolean
+yn_map = {'Y': 1,
+          'N': 0}
+
+yn_cols = ['FLAG_OWN_CAR', 'FLAG_OWN_REALTY']
+
+apps_clean[yn_cols] = apps_clean[yn_cols].replace(yn_map)
 
 # current home information
 # TODO: pick one statistic for each home info category
@@ -16,3 +24,12 @@ applications = pd.read_csv('application_train.csv', index_col="SK_ID_CURR")
 
 # age of car if owned
 # TODO: impute car age with average, if client owns a car
+
+# occupation type
+# TODO: replace with variable indicating occupation undefined
+
+# credit bureau requests
+# TODO: impute all with zero, except past year with one
+
+# annuity amount
+# TODO: infer from amt_credit and amt_goods_price
